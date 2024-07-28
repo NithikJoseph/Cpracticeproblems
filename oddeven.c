b1fbb1630f57f9f61cee6d7ec6151dc2102ca74b
NithikJoseph <119158489+NithikJoseph@users.noreply.github.com>
2024-07-28 22:49:27 +0530

    oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int num;
+int oddeven()
+    {
+        if (num%2==0)
+            printf("the number is even");
+        else
+            printf("the number is odd");
+    }
+
+int main()
+    {   printf("Enter the number");
+        scanf("num", &d);
+        oddeven();
+    }
