ca37492010f74ab8b907123f1d405ed6adb9411b
NithikJoseph <119158489+NithikJoseph@users.noreply.github.com>
2024-07-30 20:24:14 +0530

    Swap2.c

--- Swap2.c
+++ Swap2.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int a,b;
+int swap2(){
+a=a+b;
+b=a-b;
+a=a-b;
+printf("The two numbers after swapped is:%d,%d",a,b);
+}
+int main(){
+printf("Enter the two numbers to be swapped:");
+scanf("%d%d",&a,&b);
+swap2();
+}
