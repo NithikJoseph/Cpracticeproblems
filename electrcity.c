92b085f52aa0fbe579e3d338e2d3d775f0bb0c34
NithikJoseph <119158489+NithikJoseph@users.noreply.github.com>
2024-07-31 20:11:40 +0530

    electrcity.c

--- electrcity.c
+++ electrcity.c
@@ -4,15 +4,16 @@ int bill(){
   if(units<=200){
     price=units*5;
   }
-else(units<=300){
-price=200*5+(7*(units-200));
+  else if(units<=300){
+    price=200*5+(7*(units-200));
   }
-else{
+  else{
 price=200*5+7*100+(10*(units-300));
+}
 }
 int main(){
   printf("Enter the total number of units used:");
   scanf("%d",&units);
   bill();
-  printf("The total cost for %d units is ₹%d",units,price");
+  printf("The total cost for %d units is ₹%d",units,price);
 }
