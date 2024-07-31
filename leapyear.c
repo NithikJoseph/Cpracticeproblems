00ef74579d3db13277cd2cee80b31b37387a0c96
NithikJoseph <119158489+NithikJoseph@users.noreply.github.com>
2024-07-31 20:32:02 +0530

    leapyear.c

--- leapyear.c
+++ leapyear.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+int year;
+int leap(){
+  if (year%400==0){
+    printf("The entered year %d is leap year",year);
+  }
+    else if(year%4==0){
+    printf("The entered year %d is leap year",year);
+  }
+    else if (year%100==0){
+    printf("The entered year %d is leap year",year);
+    }
+    else
+    printf("The entered year %d is not leap year",year);
+}
+int main(){
+  printf("enter the year:");
+  scanf("%d",&year);
+  leap(); 
+}
